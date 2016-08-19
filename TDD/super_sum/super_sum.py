57139123375c86e864814cfc11047942dd02f5e1
Luke <mugapedia@gmail.com>
2016-08-19 15:48:44 +0300

    Fixes a bug in super_sum.py that caused test case to fail when function is called without an argument

--- TDD/super_sum/super_sum.py
+++ TDD/super_sum/super_sum.py
@@ -4,8 +4,13 @@ def super_sum(*args):
     
     print(args)
     
-    if args == None:
+    if len(args) == 0:
         raise TypeError('Function expects a list of numbers. None passed')
+    elif len(args) == 1 and type(args[0]) == list and len(args[0]) == 0:
+        '''
+            Case of a single empty list
+        '''
+        raise TypeError('Function expects a list populated with integers. Empty list passed')
     else:
         for elem in args:
             if type(elem) == str:
