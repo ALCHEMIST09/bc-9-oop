6858a477c75a993a1d3697b57e5b6ff6d8e3f9d3
Luke <mugapedia@gmail.com>
2016-08-21 06:59:09 +0300

    Fixes bugs in test cases. Adds docstring to NotesApplication

--- src/notes/notes.py
+++ src/notes/notes.py
@@ -1,12 +1,34 @@
 class NotesApplication(object):
-    def __init__(self, author, notes=[]):
-        self.author = author
-        self.notes = notes
+    def __init__(self, author, notes):
+        '''
+            Initialize the NotesApplication class with two instance properties.
+        '''
+        if author == None or notes == None:
+            raise TypeError('NotesApplication constructor expects two arguments. None given')
+        elif type(notes) != list:
+            raise TypeError('The second parameter passed to NotesApplication constructor should be a list')
+        else:
+            self.author = author
+            self.notes = notes
         
     def create(self, new_content):
-        self.notes.append(new_content)
+        '''
+            Method takes in a single argument as a note and adds
+            it to the notes property of the class
+        '''
+        if new_content == None:
+            raise TypeError('Method expects string argunment. None given')
+        elif new_content.strip() == '':
+            raise TypeError('Method expects a non-empty string as an argument. Empty string given')
+        else:
+            self.notes.append(new_content)
         
     def list(self):
+        '''
+            Loop through all entries in the notes property returning each entry 
+            using a pre-specified format: Note ID, note content, and note author
+            all on separate lines
+        '''
         for k, v in enumerate(self.notes):
             line  = str(k) + "\n"
             line += v + "\n"
@@ -14,6 +36,11 @@ class NotesApplication(object):
             print(line)
             
     def search(self, search_text):
+        '''
+            Search for a particular string in all the entries in the note list.
+            Return all the note entries that contain the particular string using
+            a pre-specified format
+        '''
         for k,v in enumerate(self.notes):
             if v.find(search_text) != -1:
                 result  = "Showing results for '" + search_text + "'\n"
@@ -23,22 +50,41 @@ class NotesApplication(object):
                 print(v)
     
     def get(self, note_id):
-        return self.notes[note_id]
+        '''
+            Look up a note in the notes propety using a user-supplied ID
+            that matches the index location of the particular note in the
+            note list property
+        '''
+        if note_id >= len(self.notes):
+            # IndexError
+            raise TypeError('Note ID is not in the list of notes')
+        else:
+            return self.notes[note_id]
                 
     def delete(self, note_id):
+        '''    
+            Given an ID that identifies a particular note in the notes
+            list property, delete that note from the list
+        '''
         self.notes.pop(note_id)
         
     def edit(self, note_id, new_content):
+        '''
+            Given an ID that identifies a particular note in the note list
+            property and content as another parameter, change the content
+            currently residing at the location referenced by the note_id argument
+            to one passed in as "new_content" argument
+        '''
         self.notes[note_id] = new_content
 
-luke_notes = ['bootcamp days at andela', 'managed passed the andela interviews']            
-np = NotesApplication('Luke', luke_notes)
-np.create('pushing stuff to my remote repositroy in github')
-np.create('andela tech talent acceleration program')
-print(np.list())
-print()
-print(np.search('andela'))
-np.edit(2, 'not so cool stuff')
-print(np.list())
+#luke_notes = ['bootcamp days at andela', 'managed passed the andela interviews']            
+#np = NotesApplication('Luke', luke_notes)
+#np.create('pushing stuff to my remote repositroy in github')
+#np.create('andela tech talent acceleration program')
+#print(np.list())
+#print()
+#print(np.search('andela'))
+#np.edit(2, 'not so cool stuff')
+#print(np.list())
   
         
--- src/notes/notes_test.py
+++ src/notes/notes_test.py
@@ -1,4 +1,5 @@
 import unittest
+import inspect
 from notes import NotesApplication
 
 class NotesApplicationTest(unittest.TestCase):
@@ -6,37 +7,96 @@ class NotesApplicationTest(unittest.TestCase):
         Test Class for Notes Application
     '''
     
-    def test_note_object_can_be_created(self):
+    def test_create_notes_object(self):
+        '''
+            Test note_object can be instantiated from NotesApplication
+        '''
+        author = 'Bootcamper'
+        notes = ['Now I know how to write various kinds of unit tests', 'I have made Git to part of my process while creating any application']
+        noteApp = NotesApplication(author, notes)
+    
+    def test_note_object_is_instance_of_notes_application_class(self):
         '''
             Test if a note taking object can be instantiated from NoteApplication
         '''
         author_name = 'Default Author'
-        notes_from_author = []
+        notes_from_author = ['week two of bootcamp']
         noteApp = NotesApplication(author_name, notes_from_author)
         self.assertIsInstance(noteApp, NotesApplication, msg='Object not an instance of NoteApplication Class')        
     
-    def test_no_constructor_passed_empty_args(self):
+    def test_constructor_passed_empty_args(self):
         '''
             Test for case where both constructor arguments are empty strings
         '''
-        noteApp = NotesApplication('', '')
-        self.assertEqual(noteApp.author, 'Default Author', msg='NotesApplication constructor expects two arguments. Empty Values Passed')
-        
-    def test_no_constructor_arg_passed(self):
+        with self.assertRaises(TypeError):
+            noteApp = NotesApplication()
+            
+    def test_constructor_not_passed_author_name(self):
         '''
-            Test case for initializing NoteApplication constructor without any argument
+            Test constructor was not given an author name
         '''
-        noteApp = NotesApplication()
-        self.assertNotIsInstance(noteApp, NotesApplication, msg='NotesApplication constructor expects two arguments. None passed')
+        author = ''
+        notes_list = ['version control for everyone']
+        noteApp = NotesApplication(author, notes_list)
+        self.assertEqual(noteApp.author, '', msg='Constructor missing name of author. Name of author required as first argument')        
         
-    def test_invalid_args_for_constructor(self):
-        author_name = 'Name'
-        author_notes = []
-        noteApp = NotesApplication(author_name, author_notes)
-        self.assertEqual(type(noteApp.author), str, msg='NotesApplication constructor expects the first argument to be a string')
-        self.assertEqual(type(noteApp), list, msg='NotesApplication constructor expects second argument to be list')
         
+    def test_notes_list_param_not_a_list(self):
+        '''
+            Test notes parameter is of type list
+        '''
+        author = 'Default Author'
+        notes_list = ['this is note a list, it is called a tuple', 'this data structure is index from zero']
+        noteApp = NotesApplication(author, notes_list)
+        self.assertEqual(type(noteApp.notes), list, msg='Second parameter of NotesApplication should be a list.')
+        with self.assertRaises(TypeError):
+            author = 'Another Author'
+            notes_list = ''
+            noteApp = NotesApplication(author, notes_list)   
     
+    def test_constructor_missing_one_argument(self):
+        '''
+            Test second parameter passed to constructor not a list
+        '''
+        with self.assertRaises(TypeError):
+            author = 'New Author'
+            noteApp = NotesApplication(author)
+            
+    def test_author_argument_is_string(self):
+        '''
+            Test to determine if author name is a string
+        '''
+        author = 'Teaching Assistant'
+        notes_list = ['Bootcamp Cohort IX at Nairobi', 'Aspiring Andelas taken through intensive training']
+        noteApp = NotesApplication(author, notes_list)
+        self.assertEqual(type(noteApp.author), str, msg="Construct expects its first argument to be a string")
+        
+    def test_create_method_called_without_argument(self):
+        '''
+            Test create method of NotesApplication class called without an argument
+        '''
+        with self.assertRaises(TypeError):
+            noteApp = NotesApplication('Luke', ['Getting closer to mastering the coding art', 'Code Complete'])
+            noteApp.create()
+    
+    def test_create_method_called_with_one_argument(self):
+        '''
+            Test create method is called with only one argument
+        '''
+        with self.assertRaises(TypeError):
+            noteApp = NotesApplication('Luke', ['Getting closer to mastering the coding art', 'Code Complete'])
+            noteApp.create('')
+            
+    def test_not_id_exists(self):
+        '''
+            Test ID used to reference a particular story is within the notes_list
+        '''
+        with self.assertRaises(TypeError):
+            noteApp = NotesApplication('Luke', ['Getting closer to mastering the coding art', 'Code Complete'])
+            noteApp.get(10)
+            
+    
+            
     
 if __name__ == '__main__':
     unittest.main()
